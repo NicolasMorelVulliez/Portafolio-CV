c4ba6f0cf47cf708ebec4bd98b7b0bf9047ff99b
Nicolas Morel Vulliez <nicomorel2002@gmail.com>
2024-03-31 20:25:55 -0300

    cinco

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ function Clients() {
       <Container>
         <FadeIn className="flex items-center gap-x-8">
           <h2 className="text-center font-display text-3xl font-semibold tracking-wider text-white sm:text-left">
-          Tech Stack
+            Tech Stack
           </h2>
           <div className="h-px flex-auto bg-neutral-800" />
         </FadeIn>
@@ -59,13 +59,13 @@ function Clients() {
         </FadeInStagger>
       </Container>
     </div>
-  )
+  );
 }
 
 function CaseStudies({
   caseStudies,
 }: {
-  caseStudies: Array<MDXEntry<CaseStudy>>
+  caseStudies: Array<MDXEntry<CaseStudy>>;
 }) {
   return (
     <>
@@ -73,8 +73,8 @@ function CaseStudies({
         title="My Proyects"
         className="mt-24 sm:mt-32 lg:mt-40"
       >
-        <p >
-          You can check more on my github 
+        <p>
+          You can check more on my github
         </p>
       </SectionIntro>
       <Container className="mt-16">
@@ -117,63 +117,12 @@ function CaseStudies({
         </FadeInStagger>
       </Container>
     </>
-  )
+  );
 }
 
-{/*function Services() {
-  return (
-    <>
-      <SectionIntro
-        eyebrow="Services"
-        title="We help you identify, explore and respond to new opportunities."
-        className="mt-24 sm:mt-32 lg:mt-40"
-      >
-        <p>
-          As long as those opportunities involve giving us money to re-purpose
-          old projects — we can come up with an endless number of those.
-        </p>
-      </SectionIntro>
-      <Container className="mt-16">
-        <div className="lg:flex lg:items-center lg:justify-end">
-          <div className="flex justify-center lg:w-1/2 lg:justify-end lg:pr-12">
-            <FadeIn className="w-[33.75rem] flex-none lg:w-[45rem]">
-              <StylizedImage
-                src={imageLaptop}
-                sizes="(min-width: 1024px) 41rem, 31rem"
-                className="justify-center lg:justify-end"
-              />
-            </FadeIn>
-          </div>
-          <List className="mt-16 lg:mt-0 lg:w-1/2 lg:min-w-[33rem] lg:pl-4">
-            <ListItem title="Web development">
-              We specialise in crafting beautiful, high quality marketing pages.
-              The rest of the website will be a shell that uses lorem ipsum
-              everywhere.
-            </ListItem>
-            <ListItem title="Application development">
-              We have a team of skilled developers who are experts in the latest
-              app frameworks, like Angular 1 and Google Web Toolkit.
-            </ListItem>
-            <ListItem title="E-commerce">
-              We are at the forefront of modern e-commerce development. Which
-              mainly means adding your logo to the Shopify store template we’ve
-              used for the past six years.
-            </ListItem>
-            <ListItem title="Custom content management">
-              At Studio we understand the importance of having a robust and
-              customised CMS. That’s why we run all of our client projects out
-              of a single, enormous Joomla instance.
-            </ListItem>
-          </List>
-        </div>
-      </Container>
-    </>
-  )
-}*/}
+
 
 export const metadata: Metadata = {
-  description:
-    'We are a development studio working at the intersection of design and technology.',
 }
 
 export default async function Home() {
@@ -196,9 +145,6 @@ export default async function Home() {
       <Clients />
 
       <CaseStudies caseStudies={caseStudies} />
-      
-      {/* <Services /> */}
-      
 
       <ContactSection />
     </>
